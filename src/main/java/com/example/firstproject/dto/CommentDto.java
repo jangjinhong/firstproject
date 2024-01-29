578892095280e3e737a9570a1f6c258032720bcd
jangjinhong <honge1122@naver.com>
2024-01-29 19:51:06 +0900

    chap.15 댓글 REST API 구현(1/2)

---
+++ src/main/java/com/example/firstproject/dto/CommentDto.java
@@ -0,0 +1,32 @@
+package com.example.firstproject.dto;
+
+import com.example.firstproject.entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+// 보여줄 데이터를 담음. 반환 JSON 데이터 전송
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @ToString
+public class CommentDto {
+    private Long id;
+    private Long articleId;
+    private String nickname;
+    private String body;
+
+    /*
+    * 정적 메서드
+    *   - 객체 생성 없이 호출가능한 메서드
+    *   - 이러한 방식으로 객체를 생성하는 메서드를 "생성 메서드"라고 한다.
+     */
+    public static CommentDto createCommentDto(Comment comment) {
+        return new CommentDto(
+                comment.getId(),
+                comment.getArticle().getId(),
+                comment.getNickname(),
+                comment.getBody()
+        );
+    }
+}
---
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -0,0 +1,25 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.entity.Comment;
+import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CommentService {
+    @Autowired CommentRepository commentRepository;
+    @Autowired ArticleRepository articleRepository;
+
+    public List<CommentDto> comments(Long articleId) {
+        // 댓글 조회-> (엔티티->DTO) 반환 -> 결과 반환
+        return commentRepository.findByArticleId(articleId)
+                .stream()
+                .map(comment -> CommentDto.createCommentDto(comment))
+                .collect(Collectors.toList());
+    }
+}
