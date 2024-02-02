7ff5780ac2ac1c2d5463a9d53d915ead5c1eb89a
jangjinhong <honge1122@naver.com>
2024-02-02 21:05:31 +0900

    chap.16 댓글 목록 조회

--- src/main/java/com/example/firstproject/repository/PizzaRepository.java
+++ src/main/java/com/example/firstproject/repository/PizzaRepository.java
@@ -2,7 +2,6 @@ package com.example.firstproject.repository;
 
 import com.example.firstproject.entity.Pizza;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 public interface PizzaRepository extends CrudRepository<Pizza, Long> {
 }
